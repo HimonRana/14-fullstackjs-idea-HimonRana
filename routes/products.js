d932bf2786a10ccd22ddc93e53814377f9d0b2fe
Himon Rana <himon.rana@chasacademy.se>
2018-09-10 12:46:50 +0200

    basic route-test

--- routes/products.js
+++ routes/products.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
 // Lägg till minst 5 produkter av minst två olika kategorier i databasen. 
 // En produkt skall ha en beskrivning, bild, kategori, pris, lagerstatus.
+
+// @route   GET products/test
+// @dec     Test products route
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: 'products works' }));
+
+module.exports = router;
