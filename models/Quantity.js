ae7f34adb4afec6ee9a10d70b5da98f8c253d13e
Himon Rana <himon.rana@chasacademy.se>
2018-09-14 16:14:30 +0200

    create models and fill product

---
+++ models/Quantity.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const QuantitySchema = new Schema({
+
+});
+
+module.exports = Quantity = mongoose.model('INSERT', QuantitySchema);
