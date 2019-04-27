96f214ac362d022d364bcb1cee03f31cd2a331c8
HimonRana <himon.rana@chasacademy.se>
2019-04-27 15:08:35 +0200

    add grid to latestproducts

--- client/src/components/products/LatestProducts.js
+++ client/src/components/products/LatestProducts.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default class LatestProducts extends Component {
+import { Card, Image, Header } from "semantic-ui-react";
+import { getProducts } from "../../actions/productActions";
+import ProductItem from "./ProductItem";
+
+class LatestProducts extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount = () => {
+    this.props.getProducts();
+  };
+
   render() {
-    return <div>Latest Products here</div>;
+    const { products } = this.props;
+    console.log(products);
+
+    let latestProducts = products.slice(0, 9).map(product => (
+      <Card className="card-product-latest">
+        <Link to={`/product/${product._id}`}>
+          <div className="products-img-container">
+            <Image alt={product.title} src={product.productImg} />
+          </div>
+        </Link>
+      </Card>
+    ));
+
+    return (
+      <div className="latest-products-container">
+        <Header>Latest Products</Header>
+        <div className="latest-products-content">{latestProducts}</div>
+      </div>
+    );
   }
 }
+
+const mapStateToProps = state => ({
+  products: state.product.products
+});
+
+export default connect(
+  mapStateToProps,
+  { getProducts }
+)(LatestProducts);
