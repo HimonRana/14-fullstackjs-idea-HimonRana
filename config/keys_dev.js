b384c1488610d58e743e5eb263300e4d0482ea4c
HimonRana <himon.rana@chasacademy.se>
2019-05-14 19:55:37 +0200

    removed keys_dev from gitignore

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb://himon123:himon123@himonscluster-shard-00-00-aqybo.mongodb.net:27017,himonscluster-shard-00-01-aqybo.mongodb.net:27017,himonscluster-shard-00-02-aqybo.mongodb.net:27017/test?ssl=true&replicaSet=HimonsCluster-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret",
+  STRIPE_SECRET_KEY: "sk_test_Xs0sj8rWEPdcEJDuNW6pEWMb009IRQ7nca"
+};
